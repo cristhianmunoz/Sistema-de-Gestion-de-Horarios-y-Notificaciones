d846d5d20cc4df1f5212ea0a62a97de5f67644bf
Erick Mayorga <erick.mayorga@epn.edu.ec>
2023-02-23 20:34:43 -0500

    Update models.py

--- gestion_voluntarios/models.py
+++ gestion_voluntarios/models.py
@@ -5,3 +5,4 @@ class Voluntario(models.Model):
     nombre = models.CharField(max_length=20, default='')
     apellido = models.CharField(max_length=20, default='')
     edad = models.IntegerField(default=0)
+
