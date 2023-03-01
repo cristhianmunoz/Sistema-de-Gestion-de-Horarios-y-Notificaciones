7e849f0c16e7202b88209397121e033a97b1db58
Giancarlo Naranjo <giancarlo.naranjo@epn.edu.ec>
2023-02-28 23:39:59 -0500

    Feat: Emergencia, servicio
    Creación del modelo "Emergencia" y pasos servicio

---
+++ gestion_voluntarios/model/emergencia_model.py
@@ -0,0 +1,5 @@
+class Emergencia:
+    def __init__(self, titulo, descripcion, habilidadesRequeridas):
+        self.titulo = titulo
+        self.descripcionEmergencia = descripcion
+        self.habilidadesRequeridas = habilidadesRequeridas
