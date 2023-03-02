e9e3ec81093607467fbd1cf2e5533b3ef58b840c
Andrés Lozano <52755615+Confectus@users.noreply.github.com>
2023-03-02 16:37:13 -0500

    Feat: Completitud de los Escenarios de Prueba de la HU1 (#2)
    Se implementaron todas las funciones asociadas a la completitud de los escenarios de prueba de la Historia de Usuario 1. Para ello, se definió un archivo de entorno environment.py con el cual se controlaron errores relacionados con el uso del contexto en los pasos de los escenarios.
    
    Adicionalmente, se crearon los controladores y las vistas que se utilizarán para desplegar la Historia de Usuario 1.
    
    ---------
    
    Co-authored-by: Erick Mayorga <erick.mayorga@epn.edu.ec>
    Co-authored-by: Francisco Encalada <francisco.encalada01@epn.edu.ec>
    Co-authored-by: Jonathan Puglla <jonathan.puglla@outlook.com>
    Co-authored-by: cristhianmunoz <cristhian.munoz@epn.edu.ec>
    Co-authored-by: Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>

--- gestion_voluntarios/model/habilidad_model.py
+++ gestion_voluntarios/model/habilidad_model.py
@@ -1,11 +1,70 @@
+import django
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from gestion_voluntarios.model.habilidad_medica_model import HabilidadMedica
-from gestion_voluntarios.model.voluntario_model import Voluntario
+
+django.setup()
 
 
 class Habilidad(models.Model):
-    titulo = models.CharField(choices=HabilidadMedica.choices)
+    titulo = models.CharField(max_length=20, choices=HabilidadMedica.choices)
     descripcion = models.CharField(max_length=200, default='')
-    horasExperiencia = models.PositiveIntegerField(default=0)
-    voluntario = models.ForeignKey(Voluntario, on_delete=models.CASCADE)
+    horas_experiencia = models.PositiveIntegerField(default=0)
+    voluntario = models.ForeignKey('Voluntario', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        # Comprobando que el título de la habilidad exista
+        if self.titulo not in HabilidadMedica:
+            return
+
+        # Comprobando que la cantidad de horas de experiencia sea correcta
+        if not self.horas_experiencia > 0:
+            return
+
+        for habilidad in self.obtener_habilidades_por_id_voluntario(self.voluntario.id):
+            # Comprobando que no se haya registrado la habilidad anteriormente para el voluntario
+            if habilidad.titulo == self.titulo and habilidad.voluntario.id:
+                return
+
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def eliminar_habilidad(id_habilidad):
+        try:
+            Habilidad.objects.get(id=id_habilidad).delete()
+            return True
+
+        except Habilidad.DoesNotExist:
+            return False
+
+    @staticmethod
+    def agregar_habilidad(habilidad):
+        try:
+            habilidad.save()
+            return True
+
+        except ValidationError:
+            return False
+
+    @staticmethod
+    def editar_habilidad(habilidad):
+        try:
+            habilidad.save()
+            return True
+
+        except ValidationError:
+            return False
+
+    @staticmethod
+    def obtener_habilidades_por_id_voluntario(id_voluntario):
+        habilidades = Habilidad.objects.filter(voluntario_id=id_voluntario)
+        return list(habilidades)
+
+    @staticmethod
+    def obtener_numero_habilidades_por_id_voluntario(id_voluntario):
+        try:
+            return Habilidad.objects.filter(voluntario_id=id_voluntario).count()
+
+        except Habilidad.DoesNotExist:
+            return 0
