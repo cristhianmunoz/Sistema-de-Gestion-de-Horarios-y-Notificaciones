2d945851fb3e175ffb2aa5aac0cf9d0d070b9a3a
Andrés Lozano <andreslozanoestrada@outlook.com>
2023-03-02 08:29:55 -0500

    Fix: Modelos comunes
    Se eliminó la carpeta de los modelos comunes.

--- common/model/periodo_model.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from common.model.dia_semana_model import DiaSemana
-
-
-class Periodo(models.Model):
-    diaSemana = models.CharField(choices=DiaSemana.choices)
-    horaInicio = models.TimeField()
-    horaFin = models.TimeField()
